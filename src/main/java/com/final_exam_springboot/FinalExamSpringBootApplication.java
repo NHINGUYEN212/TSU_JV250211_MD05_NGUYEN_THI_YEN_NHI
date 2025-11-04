5c559f7b13d4436efa3c985a43be209fa08d9746
nguyenthiyennhi <osakahankyu212@gmail.com>
2025-11-04 11:49:38 +0900

    upload build.gradle

---
+++ src/main/java/com/final_exam_springboot/FinalExamSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.final_exam_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/final_exam_springboot/FinalExamSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_exam_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
